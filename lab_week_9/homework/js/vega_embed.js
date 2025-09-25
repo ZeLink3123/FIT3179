c8e3770e880570f1a129c76d8321e1d7c72bb63a
Joesiah <tsunetoishida03@gmail.com>
2025-09-25 20:32:54 +1000

    Added functionality to swap between maps of different years

--- lab_week_9/homework/js/vega_embed.js
+++ lab_week_9/homework/js/vega_embed.js
@@ -1,2 +1,11 @@
-var map_spec = "js/map_chloropleth_anime_popularity_2024.vg.json";
-vegaEmbed("#world-map", map_spec, {mode: "vega-lite"}).then(console.log).catch(console.warn);
+document.addEventListener("DOMContentLoaded", function() {
+  let map_spec = "js/map_chloropleth_anime_popularity_2025.vg.json";
+  vegaEmbed("#world-map", map_spec, {mode: "vega-lite"});
+});
+
+document.getElementById("map-selection").addEventListener("change", function() {
+    const year = this.value;
+    let map_spec = `js/map_chloropleth_anime_popularity_${year}.vg.json`
+    vegaEmbed("#world-map", map_spec, {mode: "vega-lite"}).then(console.log).catch(console.warn);
+});
+
