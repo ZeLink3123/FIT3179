491a2d368bcb32a80f2899ca12037606f5ee915a
Joesiah <tsunetoishida03@gmail.com>
2025-09-25 19:46:24 +1000

    Fixed errors

--- lab_week_9/homework/js/vega_embed.js
+++ lab_week_9/homework/js/vega_embed.js
@@ -1,2 +1,2 @@
-var map_spec = "js/map_chloropleth_anime_popularity.vg.json";
+var map_spec = "js/map_chloropleth_anime_popularity_2024.vg.json";
 vegaEmbed("#world-map", map_spec, {mode: "vega-lite"}).then(console.log).catch(console.warn);
